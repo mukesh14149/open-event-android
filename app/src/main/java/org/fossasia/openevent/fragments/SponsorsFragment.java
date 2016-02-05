8d626f5327f28bd06b3d252eff705711e476981e
mananwason <manan13056@iiitd.ac.in>
2016-02-05 10:40:12 +0530

    sponsor ui changed,  fixes #136

--- app/src/main/java/org/fossasia/openevent/fragments/SponsorsFragment.java
+++ app/src/main/java/org/fossasia/openevent/fragments/SponsorsFragment.java
@@ -1,17 +1,24 @@
 package org.fossasia.openevent.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.*;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
@@ -22,6 +29,7 @@ import org.fossasia.openevent.adapters.SponsorsListAdapter;
 import org.fossasia.openevent.dbutils.DataDownload;
 import org.fossasia.openevent.dbutils.DbSingleton;
 import org.fossasia.openevent.events.SponsorDownloadEvent;
+import org.fossasia.openevent.utils.RecyclerItemClickListener;
 
 /**
  * Created by MananWason on 05-06-2015.
@@ -37,7 +45,7 @@ public class SponsorsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        View view = inflater.inflate(R.layout.list_sponsors, container, false);
+        final View view = inflater.inflate(R.layout.list_sponsors, container, false);
         Bus bus = OpenEventApp.getEventBus();
         bus.register(this);
         sponsorsRecyclerView = (RecyclerView) view.findViewById(R.id.list_sponsors);
@@ -57,7 +65,24 @@ public class SponsorsFragment extends Fragment {
         });
         sponsorsListAdapter = new SponsorsListAdapter(dbSingleton.getSponsorList());
         sponsorsRecyclerView.setAdapter(sponsorsListAdapter);
-        sponsorsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        sponsorsRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+        sponsorsRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        sponsorsRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(view.getContext(), new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View itemView, int position) {
+                String sponsorUrl = dbSingleton.getSponsorList().get(position).getUrl();
+                if (!sponsorUrl.startsWith("http") || !sponsorUrl.startsWith("https")) {
+                    sponsorUrl = "http://" + sponsorUrl;
+                }
+                if (Patterns.WEB_URL.matcher(sponsorUrl).matches()) {
+                    Intent sponsorsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(sponsorUrl));
+                    startActivity(sponsorsIntent);
+                } else {
+                    Snackbar.make(view, R.string.invalid_url, Snackbar.LENGTH_LONG).show();
+                }
+            }
+        }));
+
         return view;
     }
 
@@ -68,13 +93,11 @@ public class SponsorsFragment extends Fragment {
         swipeRefreshLayout.setRefreshing(false);
         if (event.isState()) {
             sponsorsListAdapter.refresh();
-            Log.d("countersp", "Refresh done");
 
         } else {
             if (getActivity() != null) {
                 Snackbar.make(getView(), getActivity().getString(R.string.refresh_failed), Snackbar.LENGTH_LONG).show();
             }
-            Log.d("countersp", "Refresh not done");
 
         }
     }
